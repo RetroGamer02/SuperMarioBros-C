8661af438ec722a1ef945f7f71b00dd5eebd7330
Devin <70994866+RetroGamer02@users.noreply.github.com>
2022-12-31 15:15:39 -0500

    Forgot some stuff for input

--- source/Main.cpp
+++ source/Main.cpp
@@ -213,11 +213,161 @@ static void mainLoop()
     bool running = true;
     int progStartTime = SDL_GetTicks();
     int frame = 0;
+
+    //Matrix containing the name of each key. Useful for printing when a key is pressed
+	char keysNames[32][32] = {
+		"KEY_A", "KEY_B", "KEY_SELECT", "KEY_START",
+		"KEY_DRIGHT", "KEY_DLEFT", "KEY_DUP", "KEY_DDOWN",
+		"KEY_R", "KEY_L", "KEY_X", "KEY_Y",
+		"", "", "KEY_ZL", "KEY_ZR",
+		"", "", "", "",
+		"KEY_TOUCH", "", "", "",
+		"KEY_CSTICK_RIGHT", "KEY_CSTICK_LEFT", "KEY_CSTICK_UP", "KEY_CSTICK_DOWN",
+		"KEY_CPAD_RIGHT", "KEY_CPAD_LEFT", "KEY_CPAD_UP", "KEY_CPAD_DOWN"
+	};
+
+    u32 kDownOld = 0, kHeldOld = 0, kUpOld = 0; //In these variables there will be information about keys detected in the previous frame
+    
+		
+
+    Controller& controller1 = engine.getController1();
+
     while (running)
     {
         SDL_Event event;
         while (SDL_PollEvent(&event))
+
+        //Scan all the inputs. This should be done once for each frame
+		hidScanInput();
+
+		//hidKeysDown returns information about which buttons have been just pressed (and they weren't in the previous frame)
+		u32 kDown = hidKeysDown();
+		//hidKeysHeld returns information about which buttons have are held down in this frame
+		u32 kHeld = hidKeysHeld();
+		//hidKeysUp returns information about which buttons have been just released
+		u32 kUp = hidKeysUp();
+
         {
+            if (kDown & KEY_START)
+        {
+            controller1.setButtonState(BUTTON_START, true);
+        }
+        if (kUp & KEY_START)
+        {
+            controller1.setButtonState(BUTTON_START, false);
+        }
+        if (kDown & KEY_SELECT)
+        {
+            controller1.setButtonState(BUTTON_SELECT, true);
+        }
+        if (kUp & KEY_SELECT)
+        {
+            controller1.setButtonState(BUTTON_SELECT, false);
+        }
+        if (kDown & KEY_A)
+        {
+            controller1.setButtonState(BUTTON_A, true);
+        }
+        if (kUp & KEY_A)
+        {
+            controller1.setButtonState(BUTTON_A, false);
+        }
+        if (kDown & KEY_B)
+        {
+            controller1.setButtonState(BUTTON_B, true);
+        }
+        if (kUp & KEY_B)
+        {
+            controller1.setButtonState(BUTTON_B, false);
+        }
+        /*if (kDown & KEY_X)
+        {
+            XButton = 1;
+        }
+        if (kUp & KEY_X)
+        {
+            XButton = 0;
+        }
+        if (kDown & KEY_Y)
+        {
+            YButton = 1;
+        }
+        if (kUp & KEY_Y)
+        {
+            YButton = 0;
+        }*/
+        if (kDown & KEY_DLEFT)
+        {
+            controller1.setButtonState(BUTTON_LEFT, true);
+        }
+        if (kUp & KEY_DLEFT)
+        {
+            controller1.setButtonState(BUTTON_LEFT, false);
+        }
+        if (kDown & KEY_DRIGHT)
+        {
+            controller1.setButtonState(BUTTON_RIGHT, true);
+        }
+        if (kUp & KEY_DRIGHT)
+        {
+            controller1.setButtonState(BUTTON_RIGHT, false);
+        }
+        if (kDown & KEY_DUP)
+        {
+            controller1.setButtonState(BUTTON_UP, true);
+        }
+        if (kUp & KEY_DUP)
+        {
+            controller1.setButtonState(BUTTON_UP, false);
+        }
+        if (kDown & KEY_DDOWN)
+        {
+           controller1.setButtonState(BUTTON_DOWN, true);
+        }
+        if (kUp & KEY_DDOWN)
+        {
+            controller1.setButtonState(BUTTON_DOWN, false);
+        }
+        /*if (kDown & KEY_L)
+        {
+            LeftShoulder = 1;
+        }
+        if (kUp & KEY_L)
+        {
+            LeftShoulder = 0;
+        }
+        if (kDown & KEY_R)
+        {
+            RightShoulder = 1;
+        }
+        if (kUp & KEY_R)
+        {
+            RightShoulder = 0;
+        }*/
+
+        //circlePosition pos;
+
+		//Read the CirclePad position
+		/*hidCircleRead(&pos);
+
+        if (pos.dx >= 15 || pos.dx <= -15)
+        {
+            StickX = pos.dx / 30;
+        } else {
+            StickX = 0;
+        }
+        if (pos.dy >= 15 || pos.dy <= -15)
+        {
+            StickY = pos.dy / 30 *-1;
+        } else {
+            StickY = 0;
+        }*/
+
+		//Set keys old values for the next frame
+		kDownOld = kDown;
+		kHeldOld = kHeld;
+		kUpOld = kUp;
+
             switch (event.type)
             {
             case SDL_QUIT:
@@ -227,6 +377,11 @@ static void mainLoop()
                 break;
             }
         }
+
+        
+
+        
+
         //Fixme
 /*
         const Uint8* keys = SDL_GetKeyboardState(NULL);
